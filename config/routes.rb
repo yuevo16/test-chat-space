7d0a0bf60f356adea23b69bd68891e19e9da6aeb
yuya <yuevo16@gmail.com>
2020-03-20 16:30:43 +0900

    グループ新規、編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
-  root "messages#index"
+  devise_for :users
+  root "groups#index"
+  resources :users, only: [:edit, :update, :destroy]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
